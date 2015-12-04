5d44ccee127ffd0d42a86339c3d0e636c09d18f4
Mat Leonard <leonard.mat@gmail.com>
2015-12-04 15:59:58 -0800

    app binds to  now

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import os
 
 from flask import Flask, request, abort
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -127,4 +128,6 @@ option_funcs = {'--default': default_location,
 
 
 if __name__ == '__main__':
-    app.run()
+	# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
